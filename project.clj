09d4c0141779661e25b87a6ba85ee703d8f77619
Jorge Zaccaro <jorgezaccaro@gmail.com>
2020-09-26 18:31:49 -0500

    remove lein-figwheel options. reorder sections

--- project.clj
+++ project.clj
@@ -1,41 +1,31 @@
 (defproject app "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring-server "0.5.0"]
-                 [reagent "0.10.0" :exclusions [cljsjs/react cljsjs/react-dom]]
-                 [reagent-utils "0.3.3"]
-                 [ring "1.8.1"]
-                 [ring/ring-defaults "0.3.2"]
-                 [hiccup "1.0.5"]
-                 [yogthos/config "1.1.7"]
-                 [org.clojure/clojurescript "1.10.773"
-                  :scope "provided"]
-                 [com.bhauman/figwheel-main "0.2.11"]
-                 [metosin/reitit "0.5.1"]
-                 [metosin/jsonista "0.2.6"]
-                 [pez/clerk "1.0.0"]
-                 [venantius/accountant "0.2.5"
-                  :exclusions [org.clojure/tools.reader]]]
+  :main app.server
+  :uberjar-name "app.jar"
+  :min-lein-version "2.5.0"
 
   :plugins [[lein-environ "1.1.0"]
             [lein-cljsbuild "1.1.7"]
             [lein-asset-minifier "0.4.6"
              :exclusions [org.clojure/clojure]]]
 
-  :ring {:handler app.handler/app
-         :uberwar-name "app.war"}
-
-  :min-lein-version "2.5.0"
-  :uberjar-name "app.jar"
-  :main app.server
-  :clean-targets ^{:protect false}
-  [:target-path
-   [:cljsbuild :builds :app :compiler :output-dir]
-   [:cljsbuild :builds :app :compiler :output-to]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
+                 [com.bhauman/figwheel-main "0.2.11"]
+                 [reagent "0.10.0" :exclusions [cljsjs/react cljsjs/react-dom]]
+                 [reagent-utils "0.3.3"]
+                 [hiccup "1.0.5"]
+                 [ring "1.8.1"]
+                 [ring-server "0.5.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [metosin/reitit "0.5.1"]
+                 [metosin/jsonista "0.2.6"]
+                 [yogthos/config "1.1.7"]
+                 [pez/clerk "1.0.0"]
+                 [venantius/accountant "0.2.5"
+                  :exclusions [org.clojure/tools.reader]]]
 
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :resource-paths ["resources" "target/cljsbuild"]
@@ -44,52 +34,11 @@
   [[:css {:source "resources/public/css/site.css"
           :target "resources/public/css/site.min.css"}]]
 
-  :cljsbuild
-  {:builds {:min
-            {:source-paths ["src/cljs" "src/cljc" "env/prod/cljs"]
-             :compiler
-             {:target :bundle
-              :bundle-cmd {:none ["npx" "webpack" "--mode=development"]
-                           :default ["npx" "webpack" "--mode=production"
-                                     "target/cljsbuild/public/js/app-raw.js" "-o"
-                                     "target/cljsbuild/public/js/app.js"]}
-              :output-to        "target/cljsbuild/public/js/app-raw.js"
-              :output-dir       "target/cljsbuild/public/js"
-              :source-map       "target/cljsbuild/public/js/app.js.map"
-              :optimizations :advanced
-              :infer-externs true
-              :pretty-print  false}}
-            :app
-            {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-             :figwheel {:on-jsload "app.core/mount-root"}
-             :compiler
-             {:main "app.dev"
-              :asset-path "/js/out"
-              :output-to "target/cljsbuild/public/js/app.js"
-              :output-dir "target/cljsbuild/public/js/out"
-              :source-map true
-              :optimizations :none
-              :pretty-print  true}}
-
-
-
-            }
-   }
-
-  :figwheel
-  {:http-server-root "public"
-   :server-port 3449
-   :nrepl-port 7002
-   :nrepl-middleware [cider.piggieback/wrap-cljs-repl
-                      ]
-   :css-dirs ["resources/public/css"]
-   :ring-handler app.handler/app}
-
-
   :aliases {"fig:run" ["trampoline" "run" "-m" "figwheel.main"]
             "fig:dev" ["fig:run" "--" "--build" "dev" "--repl"]}
 
-  :profiles {:dev {:repl-options {:init-ns app.repl}
+  :profiles {:dev {:env {:dev true}
+                   :repl-options {:init-ns app.repl}
                    :dependencies [[cider/piggieback "0.5.1"]
                                   [binaryage/devtools "1.0.2"]
                                   [ring/ring-mock "0.4.0"]
@@ -97,22 +46,28 @@
                                   [prone "2020-01-17"]
                                   [figwheel-sidecar "0.5.20"]
                                   [nrepl "0.8.0"]
-                                  [pjstadig/humane-test-output "0.10.0"]
-                                  
- ]
-
+                                  [pjstadig/humane-test-output "0.10.0"]]
                    :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.20"]
-]
-
                    :injections [(require 'pjstadig.humane-test-output)
-                                (pjstadig.humane-test-output/activate!)]
-
-                   :env {:dev true}}
-
+                                (pjstadig.humane-test-output/activate!)]}
              :uberjar {:hooks [minify-assets.plugin/hooks]
                        :source-paths ["env/prod/clj"]
                        :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
                        :env {:production true}
                        :aot :all
-                       :omit-source true}})
+                       :omit-source true}}
+
+  :cljsbuild {:builds {:min
+                       {:source-paths ["src/cljs" "src/cljc" "env/prod/cljs"]
+                        :compiler
+                        {:target :bundle
+                         :bundle-cmd {:none ["npx" "webpack" "--mode=development"]
+                                      :default ["npx" "webpack" "--mode=production"
+                                                "target/cljsbuild/public/js/app-raw.js" "-o"
+                                                "target/cljsbuild/public/js/app.js"]}
+                         :output-to        "target/cljsbuild/public/js/app-raw.js"
+                         :output-dir       "target/cljsbuild/public/js"
+                         :source-map       "target/cljsbuild/public/js/app.js.map"
+                         :optimizations :advanced
+                         :infer-externs true
+                         :pretty-print  false}}}})
